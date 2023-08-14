84eb43328988a63a1c901f51bff32be554c96a2b
unknown <akshatupadhyay2@gmail.com>
2023-08-14 20:07:30 +0530

    rearranged layout structure and sign up form added

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,17 +1,54 @@
-import Tabs from "./Tabs";
-import styles from "./LogIn.module.css";
-import image from '../assets/logInBackground.svg';
+import * as React from "react";
+import Tab from "@mui/material/Tab";
+import TabList from "@mui/lab/TabList";
+import logInImage from "../assets/logInBackground.svg";
+import signUpImage from '../assets/signUpBackground.svg';
+import Box from "@mui/material/Box";
+import TabContext from "@mui/lab/TabContext";
+import TabPanel from "@mui/lab/TabPanel";
+import Divider from "@mui/material/Divider";
+import LogInForm from "./LogInForm";
+import { useState } from "react";
+import PatientSignUp from "./PatientSignUp";
 
 const LogIn = () => {
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.imageWrapper}>
-        <img src={image}/>
-      </div>
-      <div className={styles["form-wrapper"]}>
-        <Tabs />
-      </div>
-    </div>
+    <TabContext value={value} >
+      <Box sx={{ display: "flex", justifyContent: 'space-around',width :'100%' }}>
+        <Box sx={{ width: {xs:'0',md:"60%"} }}></Box>
+        <Box sx={{ width: {xs:'100%',md:'40%'}}}>
+          <TabList
+            variant="fullWidth"
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            sx={{overflow :'hidden',maxHeight :'50px',width:{xs:'93%',md:'90%',lg:'75%'},border: 1, borderColor: "divider", p:0, m:{xs:'auto',md:'0'},borderRadius:'4px'}}
+          >
+            <Tab label="LOGIN" value="1" wrapped/>
+            <Divider orientation="vertical" sx={{ height: 50 }} />
+            <Tab label="PATIENT SIGN UP" value="2" wrapped/>
+            <Divider orientation="vertical" sx={{ height: 50 }} />
+            <Tab label="DOCTOR SIGN UP" value="3" wrapped />
+          </TabList>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex",justifyContent: 'space-around',width :'100%' }}>
+        <Box sx={{display:{xs:'none',md:'flex'},width: {xs: '0',md:'60%'}}}>
+          <img src={value === '1' ? logInImage : signUpImage} style={{ width: '80%' }} />
+        </Box>
+        <Box sx={{ width: {xs:'100%',md:'40%'} }}>
+          <TabPanel value="1"  sx={{ width:{xs:'93%',md:'90%',lg:'75%'}, p : 0, m:{xs:'auto',md:'0'}, mt: {xs:'3rem',md:'15px'}, boxShadow: 2, borderRadius:'4px'}}>
+            <LogInForm />
+          </TabPanel>
+          <TabPanel value="2"  sx={{ width:{xs:'93%',md:'90%',lg:'75%'}, p : 0, m:{xs:'auto',md:'0'}, mt: {xs:'3rem',md:'15px'}, boxShadow: 2, borderRadius:'4px'}}><PatientSignUp/></TabPanel>
+          <TabPanel value="3">DOCTOR SIGN UP</TabPanel>
+        </Box>
+      </Box>
+    </TabContext>
   );
 };
 
--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -3,24 +3,31 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 const LogInForm = () => {
+  const handleInput = () => [];
+
   return (
     <Box
-      className={styles.wrapper}
-      sx={{ boxShadow: 2, pl: 4, pr: 6, pt: 2, pb: 2 }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
       <TextField
+        onChange={handleInput}
         fullWidth
-        label="Email or Mobile Number*"
+        required
+        label="Email or Mobile Number"
         id="fullWidth"
-        className={styles.input}
-        sx={{ mb:4}}
+        sx={{ mb: "2rem", mt: "1.1rem", width: "70%" }}
       />
       <TextField
         fullWidth
         label="Password"
+        required
         id="fullWidth"
-        className={styles.input}
-        sx={{ mb:4}}
+        sx={{ mb: "2rem", width: "70%" }}
       />
 
       <Box
@@ -28,16 +35,20 @@ const LogInForm = () => {
           display: { xs: "flex" },
           justifyContent: "space-between",
           alignItems: "center",
-          ml:2,
-          mr:-2
+          width: "70%",
         }}
       >
         <Button variant="contained" href="#contained-buttons">
           Log In
         </Button>
-        <a href="">Forgot Password</a>
+        <a href="">Forgot Password?</a>
       </Box>
-      <Box sx={{ m: 4 }}>
+      <Box
+        sx={{
+          m: 4,
+          width: "70%",
+        }}
+      >
         Don't have an account?
         <a href="">Sign up</a>
       </Box>
