09ea5d11db53dee57f115cc982c5521dc02f57aa
unknown <akshatupadhyay2@gmail.com>
2023-08-22 14:56:45 +0530

    added doctors card

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import  Home  from "./pages/home/Home";
 import Specialities from "./pages/specialities/Specialities";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://my-doctors.net:8090/specializations').then((res)=>{
+      return res.json();
+    }).then ((data)=>{
+      console.log(data.data);
+    })
+  },[]);
+
   return (
     <BrowserRouter>
       <Box>
--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -7,7 +7,7 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import BoneMarrowImage from "../assets/boneMarrow.svg";
-import { useEffect, useState } from "react";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const specialityHeaderStyles = {
   color: "#3f51b5",
@@ -16,7 +16,7 @@ const specialityHeaderStyles = {
   fontWeight: "bold",
 };
 
-const cardWrapperStyles = {
+const SpecialitiesCardWrapperStyles = {
   display: "grid",
   gridTemplateColumns: {
     xs: "auto",
@@ -24,6 +24,16 @@ const cardWrapperStyles = {
     md: "auto auto auto",
   },
   gap: "54px",
+};
+
+const DoctorsCardWrapperStyles = {
+  display: "grid",
+  gridTemplateColumns: {
+    xs: "auto",
+    sm: "auto auto",
+    md: "auto auto auto",
+  },
+  gap: "35px",
 };
 
 const specialitiesCardStyles = {
@@ -45,7 +55,6 @@ const doctorsCardStyles = {
 };
 
 const Doctors = () => {
-  useEffect(() => {});
 
   const specialitiesCard = (
     <React.Fragment>
@@ -58,39 +67,35 @@ const Doctors = () => {
 
   const doctorsCard = (
     <React.Fragment>
-      <Box sx={{ display: "flex", width: "100%" }}>
-        <Box sx={{ width: "20%" }}>
-          <Box sx={{ width: "80px", height: "80", borderRadius: "50%" }}>
-            <img src={BoneMarrowImage} style={{ width: "100%" }} />
+      <Box sx={{ display: "flex", width: "100%", justifyContent:'space-between' }}>
+        <Box sx={{ width: "20%", mr:'2rem' }}>
+          <Box sx={{}}>
+            <AccountCircleIcon color='disabled' sx={{fontSize:'90px'}}/>
           </Box>
         </Box>
 
-        <Box sx={{ width: "80%" }}>
+        <Box sx={{ width: "80%" , display:'flex', flexDirection:'column', mt:'0.5rem'}}>
           <Box>
-            <Typography variant="h6">Dr. Dusty Huel</Typography>
-            <Typography>Master in Cardiovascular Risk Factors</Typography>
-            <Typography>Bachelor of Dental Surgery</Typography>
-            <Typography>Bachelor of Medicine</Typography>
-            <Typography>Critical Care Medicine</Typography>
-            <Typography>8 years experience</Typography>
+            <Typography variant="subtitle2" sx={{fontWeight:'600'}}>Dr. Dusty Huel</Typography>
+            <Typography variant="body2" sx={{fontSize:'13px', color:'rgba(0, 0, 0, 0.54)'}}>Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine </Typography>
+            <Typography variant="body2" sx={{fontSize:'13px', color:'rgba(0, 0, 0, 0.54)'}}>Critical Care Medicine</Typography>
+            <Typography variant="body2" sx={{fontSize:'13px', color:'rgba(0, 0, 0, 0.54)'}}>8 years experience</Typography>
           </Box>
 
-          <Box sx={{ display: 'flex' ,width: "100%" }}>
-            <Box sx={{ width: "50%" ,display: 'flex', flexDirection:'column'}}>
-              <Typography variant="h6">Hospital</Typography>
-              <Typography>Languages</Typography>
-              <Typography>Next available</Typography>
-            </Box>
-
-            <Box sx={{ width: "50%" ,display: 'flex', flexDirection:'column'}}>
-              <Typography>Master in Cardiovascular Risk Factors</Typography>
-              <Typography>Bachelor of Medicine</Typography>
-              <Typography>8 years experience</Typography>
-            </Box>
+          <Box sx={{ display: "grid", gridGap:"0.2rem 1rem", gridTemplateRows:'auto auto auto', gridTemplateColumns:'auto 1fr', mt:'1rem' }}>
+              <Typography variant="h6" sx={{fontSize:'13px'}}>Hospital</Typography>
+              <Typography variant="body2" sx={{fontSize:'13px', color:'rgba(0, 0, 0, 0.54)'}}>Not available</Typography>
+              <Typography sx={{fontSize:'13px'}}>Languages</Typography>
+              <Typography variant="body2" sx={{fontSize:'13px', color:'rgba(0, 0, 0, 0.54)'}}>Hindi, Dogri and Kashmiri</Typography>
+              <Typography sx={{fontSize:'13px'}}>Next available</Typography>
+              <Typography variant="body2" sx={{fontSize:'13px', color:'rgba(0, 0, 0, 0.54)'}}>Not available</Typography>
           </Box>
         </Box>
       </Box>
-      <Box>jjkn</Box>
+      
+       <Box sx={{display:'flex', gap:'0.5rem', padding:'0.8rem', alignItems:'flex-start', ml:'1rem'}}>
+           <Button variant="outlined" sx={{borderRadius:'25px', color:'#3f51b5', border:'1px solid rgba(63, 81, 181, 0.5)', fontSize:'0.8125rem'}}>BOOK APPOINTMENT</Button>
+        </Box>
     </React.Fragment>
   );
   return (
@@ -106,7 +111,7 @@ const Doctors = () => {
           </Typography>
         </Box>
 
-        <Box sx={cardWrapperStyles}>
+        <Box sx={SpecialitiesCardWrapperStyles}>
           <Box>
             <Card variant="outlined" sx={specialitiesCardStyles}>
               {specialitiesCard}
@@ -146,7 +151,7 @@ const Doctors = () => {
           <Typography variant="body1">430+ Doctors</Typography>
         </Box>
 
-        <Box sx={cardWrapperStyles}>
+        <Box sx={DoctorsCardWrapperStyles}>
           <Box>
             <Card variant="outlined" sx={doctorsCardStyles}>
               {doctorsCard}
