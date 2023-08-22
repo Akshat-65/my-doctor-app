75486fb0364cea9a6b43122e06ef39cdeb238b34
unknown <akshatupadhyay2@gmail.com>
2023-08-22 19:44:29 +0530

    fetched data of doctors in doctor component

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -2,8 +2,6 @@ import homePageImage from "../assets/homePageImage.svg";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import BoneMarrowImage from "../assets/boneMarrow.svg";
@@ -31,9 +29,10 @@ const DoctorsCardWrapperStyles = {
   display: "grid",
   gridTemplateColumns: {
     xs: "auto",
-    sm: "auto auto",
-    md: "auto auto auto",
+    sm: "1fr 1fr",
+    md: "1fr 1fr 1fr",
   },
+  gridTemplateRows: "maxContent",
   gap: "35px",
 };
 
@@ -57,6 +56,7 @@ const doctorsCardStyles = {
 
 const Doctors = () => {
   const [specializationData, setSpecializationData] = useState([]);
+  const [doctorsData, setDoctorsData] = useState([]);
 
   const getSpecializationData = async () => {
     try {
@@ -80,8 +80,43 @@ const Doctors = () => {
       console.log(error);
     }
   };
+
+  const getDoctorsData = async () => {
+    try {
+      const response = await fetch("http://my-doctors.net:8090/doctors");
+      let data = await response.json();
+      let returnedData = data.data;
+      const details = [];
+      for (let elem in returnedData) {
+        details.push({
+          name: `${returnedData[elem]["firstName"]} ${returnedData[elem]["lastName"]}`,
+          qualifications: returnedData[elem]?.profile?.["qualifications"]?.map(
+            (elem) => elem["name"]
+          ),
+          specialities: returnedData[elem]?.profile?.["specialities"]?.map(
+            (elem) => elem["name"]
+          ),
+          experience: returnedData[elem]?.profile?.experienceMonths,
+          hospital: returnedData[elem]?.profile?.["experience"]?.map((elem) => {
+            if (!elem.toYear) {
+              return elem.place;
+            }
+          }),
+          languages: returnedData[elem]?.profile?.["languages"]?.map(
+            (elem) => elem
+          ),
+        });
+      }
+      console.log(details);
+      setDoctorsData(details);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getSpecializationData();
+    getDoctorsData();
   }, []);
 
   const specialities = specializationData.map((elem) => (
@@ -98,109 +133,119 @@ const Doctors = () => {
     </Box>
   ));
 
-  const doctorsCard = (
-    <React.Fragment>
-      <Box
-        sx={{ display: "flex", width: "100%", justifyContent: "space-between" }}
-      >
-        <Box sx={{ width: "20%", mr: "2rem" }}>
-          <Box sx={{}}>
-            <AccountCircleIcon color="disabled" sx={{ fontSize: "90px" }} />
+  const doctors = doctorsData.map((elem) => (
+    <Box>
+      <Card variant="outlined" sx={doctorsCardStyles}>
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box sx={{ width: "20%", mr: "2rem" }}>
+            <Box sx={{}}>
+              <AccountCircleIcon color="disabled" sx={{ fontSize: "90px" }} />
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              width: "80%",
+              display: "flex",
+              flexDirection: "column",
+              mt: "0.5rem",
+            }}
+          >
+            <Box>
+              <Typography variant="subtitle2" sx={{ fontWeight: "600" }}>
+                {elem.name}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
+              >
+                {elem.qualifications &&
+                  elem.qualifications.map((item) => item).join(" | ")}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
+              >
+                {elem.specialities &&
+                  elem.specialities.map((item) => item).join(" | ")}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
+              >
+                {elem.experience &&
+                  `${Math.floor(elem.experience / 12)} years experience`}
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                display: "grid",
+                gridGap: "0.2rem 1rem",
+                gridTemplateRows: "auto auto auto",
+                gridTemplateColumns: "auto 1fr",
+                mt: "1rem",
+              }}
+            >
+              <Typography variant="h6" sx={{ fontSize: "13px" }}>
+                Hospital
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
+              >
+                {elem.hospital && elem.hospital.map((item) => item && item)}
+              </Typography>
+              <Typography sx={{ fontSize: "13px" }}>Languages</Typography>
+              <Typography
+                variant="body2"
+                sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
+              >
+                {elem.languages &&
+                  elem.languages.map((item) => item).join(", ")}
+              </Typography>
+              <Typography sx={{ fontSize: "13px" }}>Next available</Typography>
+              <Typography
+                variant="body2"
+                sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
+              >
+                Not available
+              </Typography>
+            </Box>
           </Box>
         </Box>
 
         <Box
           sx={{
-            width: "80%",
             display: "flex",
-            flexDirection: "column",
-            mt: "0.5rem",
+            gap: "0.5rem",
+            padding: "0.8rem",
+            alignItems: "flex-start",
+            ml: "1rem",
           }}
         >
-          <Box>
-            <Typography variant="subtitle2" sx={{ fontWeight: "600" }}>
-              Dr. Dusty Huel
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
-            >
-              Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery
-              | Bachelor of Medicine{" "}
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
-            >
-              Critical Care Medicine
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
-            >
-              8 years experience
-            </Typography>
-          </Box>
-
-          <Box
+          <Button
+            variant="outlined"
             sx={{
-              display: "grid",
-              gridGap: "0.2rem 1rem",
-              gridTemplateRows: "auto auto auto",
-              gridTemplateColumns: "auto 1fr",
-              mt: "1rem",
+              borderRadius: "25px",
+              color: "#3f51b5",
+              border: "1px solid rgba(63, 81, 181, 0.5)",
+              fontSize: "0.8125rem",
             }}
           >
-            <Typography variant="h6" sx={{ fontSize: "13px" }}>
-              Hospital
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
-            >
-              Not available
-            </Typography>
-            <Typography sx={{ fontSize: "13px" }}>Languages</Typography>
-            <Typography
-              variant="body2"
-              sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
-            >
-              Hindi, Dogri and Kashmiri
-            </Typography>
-            <Typography sx={{ fontSize: "13px" }}>Next available</Typography>
-            <Typography
-              variant="body2"
-              sx={{ fontSize: "13px", color: "rgba(0, 0, 0, 0.54)" }}
-            >
-              Not available
-            </Typography>
-          </Box>
+            BOOK APPOINTMENT
+          </Button>
         </Box>
-      </Box>
+      </Card>
+    </Box>
+  ));
 
-      <Box
-        sx={{
-          display: "flex",
-          gap: "0.5rem",
-          padding: "0.8rem",
-          alignItems: "flex-start",
-          ml: "1rem",
-        }}
-      >
-        <Button
-          variant="outlined"
-          sx={{
-            borderRadius: "25px",
-            color: "#3f51b5",
-            border: "1px solid rgba(63, 81, 181, 0.5)",
-            fontSize: "0.8125rem",
-          }}
-        >
-          BOOK APPOINTMENT
-        </Button>
-      </Box>
-    </React.Fragment>
-  );
   return (
     <>
       <img
@@ -214,9 +259,7 @@ const Doctors = () => {
           </Typography>
         </Box>
 
-        <Box sx={SpecialitiesCardWrapperStyles}>
-          {specialities}
-        </Box>
+        <Box sx={SpecialitiesCardWrapperStyles}>{specialities}</Box>
 
         <Box sx={{ pt: "1rem", textAlign: "end" }}>
           <Typography sx={{ mr: "1.3rem", fontSize: "18px" }}>
@@ -231,31 +274,7 @@ const Doctors = () => {
         </Box>
 
         <Box sx={DoctorsCardWrapperStyles}>
-          <Box>
-            <Card variant="outlined" sx={doctorsCardStyles}>
-              {doctorsCard}
-            </Card>
-          </Box>
-          <Box>
-            <Card variant="outlined" sx={doctorsCardStyles}>
-              {doctorsCard}
-            </Card>
-          </Box>
-          <Box>
-            <Card variant="outlined" sx={doctorsCardStyles}>
-              {doctorsCard}
-            </Card>
-          </Box>
-          <Box>
-            <Card variant="outlined" sx={doctorsCardStyles}>
-              {doctorsCard}
-            </Card>
-          </Box>
-          <Box>
-            <Card variant="outlined" sx={doctorsCardStyles}>
-              {doctorsCard}
-            </Card>
-          </Box>
+          {doctors}
         </Box>
       </Box>
     </>
