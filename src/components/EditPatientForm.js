f5cf0f8cf64eaa628445e7896224fa153265a364
unknown <akshatupadhyay2@gmail.com>
2023-09-07 19:13:30 +0530

    patient form added, optimised

---
+++ src/components/EditPatientForm.js
@@ -0,0 +1,224 @@
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Autocomplete from "@mui/material/Autocomplete";
+import MenuItem from "@mui/material/MenuItem";
+
+const EditPatientForm = ({
+  user,
+  name,
+  gender,
+//   patientData,
+//   handleNameChange,
+//   handleGender,
+//   handleBloodgroup,
+//   handleHouseNo,
+//   handleLocality,
+//   handleCity,
+//   handleState,
+//   handleCountry,
+//   handlePincode,
+  handleInput
+}) => {
+  const genderOptions = [
+    { value: "Male" },
+    { value: "Female" },
+    { value: "Other" },
+  ];
+
+  const handleInputChange = (e)=>{
+handleInput(e)
+  }
+
+//   const handleInputNameChange = (e) => {
+//     handleNameChange(e);
+//   };
+
+//   const handleGenderChange = (e) => {
+//     handleGender(e);
+//   };
+
+//   const handleBloodgroupChange = (e, value) => {
+//     handleBloodgroup(e, value);
+//   };
+
+//   const handleHouseNoChange = (e) => {
+//     handleHouseNo(e);
+//   };
+
+//   const handleLocalityChange = (e) => {
+//     handleLocality(e);
+//   };
+
+//   const handleCityChange = (e) => {
+//     handleCity(e);
+//   };
+
+//   const handleStateChange = (e) => {
+//     handleState(e);
+//   };
+
+//   const handleCountryChange = (e) => {
+//     handleCountry(e);
+//   };
+
+//   const handlePincodeChange = (e) => {
+//     handlePincode(e);
+//   };
+
+  return (
+    <>
+      <FormControl>
+        <InputLabel htmlFor="patient-name">Name</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          defaultValue={name}
+          label="Name"
+          name="name"
+        //   onChange={handleInputNameChange}
+        onChange={handleInputChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">Phone Number</InputLabel>
+        <OutlinedInput
+          disabled
+          id="patient-name"
+          value={user.user.contactNumber}
+          label="Phone Number"
+        />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">Email</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          value={user.user.email}
+          label="Email"
+          disabled
+        />
+      </FormControl>
+
+      <TextField
+        id="outlined-select-currency"
+        select
+        label="Gender"
+        name="gender"
+        defaultValue={gender}
+        // onChange={handleGenderChange}
+        onChange={handleInputChange}
+      >
+        {genderOptions.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.value}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">Date of birth</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          name="dob"
+          value={user.user.profile.dob}
+          label="Date of birth"
+        />
+      </FormControl>
+
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        fullWidth
+        options={bloodgroup}
+        name="bloodType"
+        // onChange={handleBloodgroupChange}
+        onChange={(e, value) => handleInputChange(e, value)}
+        renderInput={(params) => <TextField {...params} label="Bloodgroup" />}
+      />
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">House No./Street/Area</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          //   value="House No./Street/Area"
+          defaultValue="House No./Street/Area"
+          name="area"
+          label="House No./Street/Area"
+        //   onChange={handleHouseNoChange}
+          onChange={handleInputChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">Colony/Street/ Locality</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          //   value="Colony/Street/ Locality"
+          name="locality"
+          defaultValue="Colony/Street/ Locality"
+          label="Colony/Street/ Locality"
+        //   onChange={handleLocalityChange}
+          onChange={handleInputChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">City</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          //   value="City"
+          label="City"
+          name="city"
+          defaultValue="City"
+        //   onChange={handleCityChange}
+          onChange={handleInputChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">State</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          defaultValue="State"
+          //   value="State"
+          name="state"
+          label="State"
+        //   onChange={handleStateChange}
+          onChange={handleInputChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">Country</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+          //   value="Country"
+          label="Country"
+          name="country"
+          defaultValue="country"
+        //   onChange={handleCountryChange}
+          onChange={handleInputChange}
+        />
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="patient-name">Pincode</InputLabel>
+        <OutlinedInput
+          id="patient-name"
+        //   value="Pincode"
+          label="Pincode"
+          name="pincode"
+          defaultValue="Pincode"
+        //   onChange={handlePincodeChange}
+        onChange={handleInputChange}
+        />
+      </FormControl>
+    </>
+  );
+};
+
+export default EditPatientForm;
+
+const bloodgroup = ["A+", "A-", "B+", "AB-", "AB+", "O+", "O-"];
--- src/pages/PatientProfile.js
+++ src/pages/PatientProfile.js
@@ -3,12 +3,8 @@ import SideNav from "../components/SideNav";
 import Typography from "@mui/material/Typography";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import Button from "@mui/material/Button";
-import TextField from '@mui/material/TextField';
-import FilledInput from "@mui/material/FilledInput";
-import FormControl from "@mui/material/FormControl";
-import FormHelperText from "@mui/material/FormHelperText";
-import InputLabel from "@mui/material/InputLabel";
-import OutlinedInput from "@mui/material/OutlinedInput";
+import EditPatientForm from "../components/EditPatientForm";
+import { useState } from "react";
 
 // ------------------------------------styles----------------------------------------
 
@@ -35,12 +31,221 @@ const formWrapper = {
     lg: "1fr 1fr 1fr",
   },
   gap: "20px",
-  mt: "15px",
+  mt: "2rem",
 };
 
 // ------------------------------------component----------------------------------------
 
 const PatientProfile = () => {
+  const user = JSON.parse(localStorage.getItem("userContext"));
+  console.log(user.user.contactNumber);
+
+  const name = user.user?.lastName
+    ? user.user.firstName + " " + user.user.lastName
+    : user.user?.firstName || " ";
+  let nameArray = name.split(" ");
+  let firstName = nameArray[0];
+  let lastName = nameArray[1];
+  console.log(name);
+  const gender = user.user.gender[0].toUpperCase() + user.user.gender.slice(1);
+
+  const patientDetails = {
+    firstName: firstName,
+    lastName: lastName,
+    gender: gender,
+    profile: {
+      dob: user.user.profile.dob,
+      bloodType:"",
+      address: {
+        city: "N/a",
+        country: "N/a",
+        area: "N/a",
+        state: "N/a",
+        pincode: "N/a",
+        locality: "N/a",
+      },
+    },
+  };
+  const [patientData, setPatientData] = useState(patientDetails);
+
+  const handleInput = (e,value) => {
+    const { name } = e.target; 
+    const inputValue = e.target.value;
+
+    if (name === "name") {
+      console.log("name", inputValue);
+      console.log(inputValue);
+      let name = inputValue;
+      let nameArray = name.split(" ");
+      let firstName = nameArray[0];
+      let lastName = nameArray[1];
+  
+      setPatientData((prev) => ({
+        ...prev,
+        firstName: firstName,
+        lastName: lastName,
+      }));
+    }
+    else if(name === "area" || name === "locality" || name === "city" || name === "state" ||name === "pincode" ||name === "country") {
+      setPatientData((prev) => ({
+        ...prev,
+        profile: {
+          ...prev.profile,
+          address: {
+            ...prev.profile.address,
+            [name]: inputValue,
+          },
+        },
+      }));
+    }
+    else if ( name === "dob") {
+      setPatientData((prev) => ({
+        ...prev,
+        profile: {
+          ...prev.profile,
+          [name]: inputValue,
+        },
+      }));
+    }
+    else if(name === "bloodType" ){
+      value = value
+      setPatientData((prev) => ({
+        ...prev,
+        profile: {
+          ...prev.profile,
+          [name]: value,
+        },
+      }));
+    }
+    else{
+      setPatientData((prev) => ({
+        ...prev,
+        [name]: inputValue,
+      }));
+    }
+  };
+
+  console.log(patientData);
+
+
+  // const handleNameChange = (e) => {
+  //   console.log("name", e.target.value);
+  //   console.log(e.target.value);
+  //   let name = e.target.value;
+  //   let nameArray = name.split(" ");
+  //   let firstName = nameArray[0];
+  //   let lastName = nameArray[1];
+
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //     firstName: firstName,
+  //     lastName: lastName,
+  //   }));
+  // };
+
+  // const handleGender = (e) => {
+  //   console.log("gender", e.target.value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //     gender: e.target.value,
+  //   }));
+  // };
+
+  // const handleBloodgroup = (e,value)=>{
+  //   console.log("gender", value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //     bloodType: value,
+  //   }));
+  // }
+
+  // const handleHouseNo = (e)=>{
+  //   console.log("area", e.target.value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //      profile : {
+  //       ...prev.profile,
+  //       address: {
+  //         ...prev.profile.address, 
+  //         area : e.target.value
+  //       }
+  //      }
+  //   }));
+  // }
+
+  // const handleLocality = (e)=>{
+  //   console.log("locality", e.target.value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //      profile : {
+  //       ...prev.profile,
+  //       address: {
+  //         ...prev.profile.address, 
+  //         locality : e.target.value
+  //       }
+  //      }
+  //   }));
+  // }
+
+  // const handleCity = (e)=>{
+  //   console.log("city", e.target.value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //      profile : {
+  //       ...prev.profile,
+  //       address: {
+  //         ...prev.profile.address, 
+  //         city : e.target.value
+  //       }
+  //      }
+  //   }));
+  // }
+
+  // const handleState = (e)=>{
+  //   console.log("state", e.target.value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //      profile : {
+  //       ...prev.profile,
+  //       address: {
+  //         ...prev.profile.address, 
+  //         state : e.target.value
+  //       }
+  //      }
+  //   }));
+  // }
+
+  // const handleCountry = (e)=>{
+  //   console.log("country", e.target.value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //      profile : {
+  //       ...prev.profile,
+  //       address: {
+  //         ...prev.profile.address, 
+  //         country : e.target.value
+  //       }
+  //      }
+  //   }));
+  // }
+
+  // const handlePincode = (e)=>{
+  //   console.log("pincode", e.target.value);
+  //   setPatientData((prev) => ({
+  //     ...prev,
+  //      profile : {
+  //       ...prev.profile,
+  //       address: {
+  //         ...prev.profile.address, 
+  //         pincode : e.target.value
+  //       }
+  //      }
+  //   }));
+  // }
+
+
+
+
   const drawerWidth = 240;
   return (
     <Box sx={{ display: "flex", mt: { xs: "12rem", md: "9rem" } }}>
@@ -78,73 +283,22 @@ const PatientProfile = () => {
           </Box>
 
           <Box sx={formWrapper}>
-          <TextField
-          id="outlined-required"
-          label="Name"
-        />
-            {/* <FormControl>
-              <InputLabel htmlFor="component-outlined">Name</InputLabel>
-              <OutlinedInput
-                id="component-outlined"
-                defaultValue=""
-                label="Name"
-              />
-            </FormControl>
-
-            <FormControl>
-              <InputLabel htmlFor="component-outlined">Phone Number</InputLabel>
-              <OutlinedInput
-                id="component-outlined"
-                defaultValue=""
-                label="Phone Number"
-              />
-            </FormControl>
-
-            <FormControl>
-              <InputLabel htmlFor="component-outlined">Email</InputLabel>
-              <OutlinedInput
-                id="component-outlined"
-                defaultValue=""
-                label="Email"
-              />
-            </FormControl>
-
-            <FormControl>
-              <InputLabel htmlFor="component-outlined">Gender</InputLabel>
-              <OutlinedInput
-                id="component-outlined"
-                defaultValue=""
-                label="Gender"
-              />
-            </FormControl>
-
-            <FormControl>
-              <InputLabel htmlFor="component"> Date of birth</InputLabel>
-              <OutlinedInput
-                id="component"
-                defaultValue=" "
-                label="Date of birth"
-              />
-            </FormControl>
-
-            <FormControl>
-              <InputLabel htmlFor="component-outline"> Date of birth</InputLabel>
-              <OutlinedInput
-                id="component-outline"
-                defaultValue=" "
-                label="Date of birth"
-              />
-            </FormControl>
-
-            
-            <FormControl>
-              <InputLabel htmlFor="componen"> Date of birth</InputLabel>
-              <OutlinedInput
-                id="componen"
-                defaultValue=" "
-                label="Date of birth"
-              />
-            </FormControl> */}
+            <EditPatientForm
+              user={user}
+              name={name}
+              gender={gender}
+              patientData={patientData}
+              // handleNameChange={handleNameChange}
+              // handleGender={handleGender}
+              // handleBloodgroup  ={handleBloodgroup}
+              // handleHouseNo = {handleHouseNo}
+              // handleLocality={handleLocality}
+              // handleCity= {handleCity}
+              // handleState = {handleState}
+              // handleCountry = {handleCountry}
+              // handlePincode = {handlePincode}
+              handleInput = {handleInput}
+            />
           </Box>
         </Box>
       </Box>
