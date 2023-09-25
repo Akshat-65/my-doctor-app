0d1ce7140eb6922c9eede0f2859b37b6373206dd
unknown <akshatupadhyay2@gmail.com>
2023-09-25 15:44:06 +0530

    doctor details card separated

---
+++ src/components/DoctorDetailsCard.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+
+
+const DoctorDetailsCard = ({doctorsDetailsData}) => {
+    return ( 
+        <Card
+      sx={{ p: "1rem", boxShadow: 2, minWidth: { xs: "250px", sm: "480px" } }}
+    >
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }}>
+            <img src="" alt={doctorsDetailsData.name} />
+          </Avatar>
+        }
+        title={`Dr. ${doctorsDetailsData.name}`}
+        subheader={
+          doctorsDetailsData["experienceMonths"] &&
+          doctorsDetailsData["experienceMonths"]
+            ? `${Math.floor(
+                doctorsDetailsData.experienceMonths / 12
+              )} Years of experience`
+            : "No experience"
+        }
+        sx={{ mb: "1rem" }}
+      />
+      <CardContent sx={{ mb: "1rem" }}>
+        <Typography variant="body2" color="text.secondary">
+          {doctorsDetailsData && doctorsDetailsData.bio
+            ? `${doctorsDetailsData.bio}`
+            : "Bio not available"}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+     );
+}
+ 
+export default DoctorDetailsCard;
--- src/pages/DoctorDetails.js
+++ src/pages/DoctorDetails.js
@@ -1,34 +1,18 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import SideNav from "../components/SideNav";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import DoctorAccordion from "../components/DoctorAccordion";
+import DoctorDetailsCard from "../components/DoctorDetailsCard";
 
 const doctorDetailsSectionStyles = {
   display: "grid",
-  //   display: "flex",
   gridTemplateColumns: "auto",
-  //   flexDirection: "column",
   pl: "2rem",
   pt: "2rem",
   pr: "1rem",
-  //   width: "100%",
-  //   alignItems: "flex-start",
 };
 
 const DoctorDetails = () => {
@@ -90,77 +74,6 @@ const DoctorDetails = () => {
       </Typography>
   );
 
-  const doctorDetailsCard = (
-    <Card
-      sx={{ p: "1rem", boxShadow: 2, minWidth: { xs: "250px", sm: "480px" } }}
-    >
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }}>
-            <img src="" alt={doctorsDetailsData.name} />
-          </Avatar>
-        }
-        title={`Dr. ${doctorsDetailsData.name}`}
-        subheader={
-          doctorsDetailsData["experienceMonths"] &&
-          doctorsDetailsData["experienceMonths"]
-            ? `${Math.floor(
-                doctorsDetailsData.experienceMonths / 12
-              )} Years of experience`
-            : "No experience"
-        }
-        sx={{ mb: "1rem" }}
-      />
-      <CardContent sx={{ mb: "1rem" }}>
-        <Typography variant="body2" color="text.secondary">
-          {doctorsDetailsData && doctorsDetailsData.bio
-            ? `${doctorsDetailsData.bio}`
-            : "Bio not available"}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-  );
-
-  // const doctorDetailsKeys = Object.keys(doctorsDetailsData);
-  //   console.log(doctorDetailsKeys);
-
-  //   doctorDetailsKeys.map((elem)=>{
-  //     if(elem!=='name' && elem!=='experienceMonths' && elem!=="consultationFee" && elem!=='bio' ){
-  //         return(
-  //             <Accordion>
-  //         <AccordionSummary
-  //           expandIcon={<ExpandMoreIcon />}
-  //           aria-controls="panel1a-content"
-  //           id="panel1a-header"
-  //         >
-  //           <Typography>{elem}</Typography>
-  //         </AccordionSummary>
-  //          <AccordionDetails>
-  //           <Typography>
-  //             {Array.isArray(details[elem]) ? (
-  //               <ul>
-  //                 {details[elem] && details[elem].map((item, index) => (
-  //                   <li key={index}>{item}</li>
-  //                 ))}
-  //               </ul>
-  //             ) : (
-  //               details[key]
-  //             )}
-  //           </Typography>
-  //         </AccordionDetails>
-  //       </Accordion>
-  //         )
-  //     }
-  //   })
-
   return (
     <Box sx={{ display: "flex", mt: { xs: "12rem", md: "9rem" } }}>
       <SideNav />
@@ -177,7 +90,7 @@ const DoctorDetails = () => {
             <>
               {/* card wrapper */}
               <Box sx={{ display: "flex", mb: "1rem", maxWidth: "47%" }}>
-                {doctorDetailsCard}
+                <DoctorDetailsCard doctorsDetailsData ={doctorsDetailsData}/>
               </Box>
               {/* No slots available Wrapper */}
               <Box sx={{ mb: "1rem" }}>
