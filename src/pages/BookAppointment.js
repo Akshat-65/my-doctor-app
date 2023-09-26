18e0b6a5c76afc02b8d4703db03d7de9b23e0ff9
unknown <akshatupadhyay2@gmail.com>
2023-09-26 18:19:21 +0530

    formatted and cleaned book appointment component

--- src/pages/BookAppointment.js
+++ src/pages/BookAppointment.js
@@ -14,49 +14,14 @@ const BookAppointment = () => {
   const drawerWidth = 240;
   const steps = ["Patient Details", "Appointment Details", "Payment Details"];
   const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
 
   const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
   };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-
-//   const handleSkip = () => {
-//     if (!isStepOptional(activeStep)) {
-//       // You probably want to guard against something like this,
-//       // it should never occur unless someone's actively trying to break something.
-//       throw new Error("You can't skip a step that isn't optional.");
-//     }
-
-//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-//     setSkipped((prevSkipped) => {
-//       const newSkipped = new Set(prevSkipped.values());
-//       newSkipped.add(activeStep);
-//       return newSkipped;
-//     });
-//   };
-
-//   const handleReset = () => {
-//     setActiveStep(0);
-//   };
 
   let showDetails;
   if (activeStep === 0) {
@@ -73,36 +38,29 @@ const BookAppointment = () => {
       <Box
         component="main"
         sx={{
-          p: {xs:"4px",sm:"24px",md:"32px"},
-          pt:"8px",
+          p: { xs: "4px", sm: "24px", md: "32px" },
+          pt: "8px",
           flexGrow: 1,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
         }}
       >
-        {/* <Box component="section" sx={{ pl: "1rem", pr: "1rem" }}> */}
-        <Stepper activeStep={activeStep} 
- sx={{ p: {xs:"16px",sm:"24px"} }}>
-          {steps.map((label, index) => {
+        <Stepper activeStep={activeStep} sx={{ p: { xs: "16px", sm: "24px" } }}>
+          {steps.map((label) => {
             const stepProps = {};
             const labelProps = {};
-            //   if (isStepOptional(index)) {
-            //     labelProps.optional = (
-            //       <Typography variant="caption">Optional</Typography>
-            //     );
-            //   }
-            //   if (isStepSkipped(index)) {
-            //     stepProps.completed = false;
-            //   }
             return (
-              <Step key={label} {...stepProps} >
-                <StepLabel {...labelProps} ><Typography sx={{fontSize:{xs:'12px',sm:'1rem'}}}>{label}</Typography></StepLabel>
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>
+                  <Typography sx={{ fontSize: { xs: "12px", sm: "1rem" } }}>
+                    {label}
+                  </Typography>
+                </StepLabel>
               </Step>
             );
           })}
         </Stepper>
         <Box
           sx={{
-            // width:"100%",
             ml: "auto",
             mr: "auto",
             pl: { xs: "16px", sm: "24px" },
@@ -120,8 +78,6 @@ const BookAppointment = () => {
           <Box>{showDetails}</Box>
           <Box
             sx={{
-              //   display: "flex",
-              //   flexDirection: "row",
               display: "grid",
               padding: "0.4rem 0",
               columnGap: "1rem",
@@ -133,15 +89,10 @@ const BookAppointment = () => {
               color="inherit"
               disabled={activeStep === 0}
               onClick={handleBack}
-              sx={{ mr: 1 , border: '1px solid rgba(0, 0, 0, 0.23)'}}
+              sx={{ mr: 1, border: "1px solid rgba(0, 0, 0, 0.23)" }}
             >
               Back
             </Button>
-            {/* {isStepOptional(activeStep) && (
-                <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                  Skip
-                </Button>
-              )} */}
 
             <Button
               variant="contained"
@@ -156,41 +107,6 @@ const BookAppointment = () => {
             </Button>
           </Box>
         </Box>
-        {/* {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                Skip
-              </Button>
-            )}
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )} */}
       </Box>
     </Box>
     // </Box>
